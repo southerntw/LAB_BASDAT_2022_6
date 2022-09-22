be162fd6a96b3f3032b14e59f04ba8c4ef1d6a74
Chindy febryan <45525155+chindyfebryan@users.noreply.github.com>
2022-09-23 05:06:16 +0800

    praktikum1

--- H071211038/Praktikum1/nomor3.sql
+++ H071211038/Praktikum1/nomor3.sql
@@ -1,17 +1,2 @@
-CREATE DATABASE nomor3;
-
-USE nomor3;
-
-CREATE TABLE offices(
-officeCode VARCHAR (10) NOT NULL,
-city VARCHAR (50) NOT NULL,
-phone VARCHAR (50) NOT NULL,
-addressline1 VARCHAR (50) NOT NULL,
-addressline2 VARCHAR (50),
-state VARCHAR (50),
-country VARCHAR (50) NOT NULL,
-PRIMARY KEY (officeCode)
-)
-
 ALTER TABLE offices
 MODIFY phone INT (20) NOT NULL;
--- H071211038/Praktikum1/nomor4.sql
+++ H071211038/Praktikum1/nomor4.sql
@@ -1,17 +1,2 @@
-CREATE DATABASE nomor4;
-
-USE nomor4;
-
-CREATE TABLE offices(
-officeCode VARCHAR (10) NOT NULL,
-city VARCHAR (50) NOT NULL,
-phone VARCHAR (50) NOT NULL,
-addressline1 VARCHAR (50) NOT NULL,
-addressline2 VARCHAR (50),
-state VARCHAR (50),
-country VARCHAR (50) NOT NULL,
-PRIMARY KEY (officeCode)
-)
-
 ALTER TABLE offices
 DROP addressline2;
