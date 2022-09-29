5be3f6c529d9b63ccb41cbaadfa3878d31cd7e0c
taufiq53 <taufiq050503@gmail.com>
2022-09-29 11:16:29 +0800

    menambahkan tugas

---
+++ H071211062/Praktikum 2/Tugas Praktimum Basis Data 2.sql
@@ -0,0 +1,5 @@
+#SELECT * FROM offices WHERE city = "san Francisco"
+#SELECT * FROM orderdetails WHERE quantityOrdered > 70 ORDER BY orderLineNumber ASC
+#SELECT DISTINCT productLine FROM products
+#SELECT customerNumber AS `nomor customer`, customerName AS `nama customer` FROM customers WHERE customerNumber >= 100 AND customerNumber <= 150
+#SELECT * FROM customers WHERE country != "USA" AND creditLimit > 0 ORDER BY customerName ASC LIMIT 10,10
