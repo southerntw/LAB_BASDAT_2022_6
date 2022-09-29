1d3ef135ead9e798878f275422926134c8093b0c
Dirgantry Leonard Nugrah Boro <92706538+dirganoob@users.noreply.github.com>
2022-09-29 12:03:39 +0800

    delete tugas2.sql

--- H071211080/Praktikum 2/Tugas2 praktikum.sql
+++
@@ -1,9 +0,0 @@
-select * from offices where city = "San Francisco";
-
-select * from orderdetails where quantityOrdered > 70 order by orderLineNumber;
-
-select distinct productlines from products;
-
-select customerNumber as 'nomor kostumer',customerName as 'nama kostumer' from customers where customerNumber >= 100 && customerNumber <=150;
-
-select * from customers where country != "USA" order by customerName limit 10,19;
