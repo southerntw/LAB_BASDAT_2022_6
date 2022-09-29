f6b8fcc09d11b0f298a93944cd3580a327de9f1c
Chindy Febryan <45525155+chindyfebryan@users.noreply.github.com>
2022-09-29 17:28:45 +0800

    Delete H071211038/Praktikum 2 directory

--- H071211038/Praktikum 2/Tugas2.sql
+++
@@ -1,5 +0,0 @@
--- SELECT * FROM offices WHERE city = 'San Francisco';
--- SELECT * FROM orderdetails WHERE quantityOrdered>70 ORDER BY orderLineNumber ASC;
--- SELECT DISTINCT productLine FROM products;
--- SELECT customerNumber AS `nomor cust`, customerName AS `nama cust` FROM customers WHERE customerNumber>=100 AND customerNumber<=150;
--- SELECT * FROM customers WHERE country!='USA' AND creditLimit>0 ORDER BY customerName ASC LIMIT 10,10;
