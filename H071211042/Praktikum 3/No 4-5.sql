6a8369482a4ca6e5bf238a9c973275c1b57b2fa9
MuhHudriPerdana <muhhudry18@gmail.com>
2022-10-06 23:10:40 +0800

    Tugas 3

---
+++ H071211042/Praktikum 3/No 4-5.sql
@@ -0,0 +1,22 @@
+#No 4
+-- INSERT INTO buku
+-- VALUES 
+-- ('Hujan', 2016, 'Tere liye', 1),
+-- ('Sebuah Seni Untuk Bersikap Bodo Amat', 2016, 'Mark Manson', 2),
+-- ('Tapak Jejak', 2019, 'Fiersa Besari', 3),
+-- ('Dia adalah Dilanku tahun 1990', 2014, 'Pidi Baiq', 4),
+-- ('Bumi Manusia', 2018, 'Ananta Toer', 5);
+
+-- INSERT INTO pinjam
+-- VALUES
+-- ('2022-01-01 10:00:00' , 4, 1, 1, 1),
+-- ('2022-01-05 11:25:00', 7, 5, 0, 2),
+-- ('2022-02-03 08:12:00', 6, 3, 0, 3),
+-- ('2022-02-21 12:19:00', 8, 4, 1, 4),
+-- ('2022-02-28 08:03:00', 11, 2, 0, 5);
+
+#No 5
+-- INSERT INTO mahasiswa
+-- VALUES
+-- ('Herdiangga Pratama','', 'L', 15, '085111222333', 'Herdiangga@gmail.com', 'Jl. Rappokalling', 'Semester 3'),
+-- ('Michiko Tjiang', 'H071211013', 'P', 16, '081222333444', 'Michiko@gmail.com', 'Jl. Batua Raya', NULL);
