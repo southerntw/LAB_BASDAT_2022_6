9e81944904ba150b24784dd5ccbaa0fa8771d22e
CEFra <rendy.luis1395@gmail.com>
2022-10-07 03:30:33 +0800

    Praktikum 3

--- H071201042/Praktikum3/nomor1_2.sql
+++ H071201042/Praktikum3/nomor1_2.sql
@@ -1,10 +1,10 @@
 USE classicmodels;
 #Nmor1
--- INSERT INTO employees
--- VALUES
--- (1800,'Jack','Jock','x102','jackolantern@classicmodelcars.com',2,1621,'Sales Rep'),
--- (1003,'Luis','Rendy','X5393','rendy@classicmodelcars.com',1,1002,'Vice President'),
--- (1500,'Jane','Aliza','x102','janen@classicmodelcars.com',4,1621,'Sales Rep');
+INSERT INTO employees
+VALUES
+(1800,'Jack','Jock','x102','jackolantern@classicmodelcars.com',2,1621,'Sales Rep'),
+(1003,'Luis','Rendy','X5393','rendy@classicmodelcars.com',1,1002,'Vice President'),
+(1500,'Jane','Aliza','x102','janen@classicmodelcars.com',4,1621,'Sales Rep');
 
 #Nomor2
 INSERT INTO offices
