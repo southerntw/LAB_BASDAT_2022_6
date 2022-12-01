1544aaceb8b534fe989dbe1f05a97200e78cd8ba
MuhHudriPerdana <muhhudry18@gmail.com>
2022-12-01 15:32:00 +0800

    Tugas 9 & 10

---
+++ H071211042/Praktikum 10/Tugas 10.sql
@@ -0,0 +1,13 @@
+SELECT customers.customerName,CASE 
+WHEN (customers.creditLimit-SUM(orderdetails.quantityOrdered*orderdetails.priceEach)) < 0 THEN 'You are in Debt'
+WHEN (customers.creditLimit-SUM(orderdetails.quantityOrdered*orderdetails.priceEach)) > 0 THEN 'You are in Safe'
+WHEN (customers.creditLimit-SUM(orderdetails.quantityOrdered*orderdetails.priceEach)) = 0 THEN 'You are runing out of credits' END
+AS 'Are You Safe',(customers.creditLimit-SUM(orderdetails.quantityOrdered*orderdetails.priceEach)) AS Total FROM customers
+JOIN orders
+USING (CustomerNumber)
+JOIN orderdetails
+USING (OrderNumber)
+GROUP BY customers.customerName;
+
+
+
